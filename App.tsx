6dea9de1c5a457c8ed4986d43019f8ec0ae3ff80
Shivam Agarwal <99248722+shivamagarwal2510@users.noreply.github.com>
2024-03-14 16:58:40 +0530

    react native todo done

--- App.tsx
+++ App.tsx
@@ -1,118 +1,114 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState<Array<{id: number; text: string}>>([]);
+  // console.debug(todo);
+  const addTodo = () => {
+    console.log('Adding todo', todo);
+    setTodos([...todos, {id: Date.now(), text: todo}]);
+    setTodo('');
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleDeleteTodo = (id: number) => {
+    console.log('Deleting todo', id);
+    const newTodos = todos.filter(todoItem => todoItem.id !== id);
+    setTodos(newTodos);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text style={styles.heading}>Taskez</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Enter To-do"
+            placeholderTextColor={'gray'}
+            value={todo}
+            onChange={textContent => setTodo(textContent.nativeEvent.text)}
+          />
+          <TouchableOpacity
+            style={styles.inputButton}
+            onPress={addTodo}
+            activeOpacity={0.5}>
+            <Text style={styles.inputButtonText}>Go</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          {todos.map(todoItem => (
+            <View style={styles.todoItem} key={todoItem.id}>
+              <Text style={styles.todoText}>{todoItem.text}</Text>
+              <TouchableOpacity onPress={() => handleDeleteTodo(todoItem.id)}>
+                <Text style={styles.todoDelete}>Delete</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#000000fd',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  heading: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'monsterat-bold',
+    color: 'white',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+    // setting placeholder color gray
   },
-  highlight: {
-    fontWeight: '700',
+  inputStyle: {
+    width: '82%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 20,
+    color: 'white',
+  },
+  inputButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 5,
+  },
+  inputButtonText: {
+    color: 'white',
+    fontWeight: '500',
+  },
+  todoItem: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#333',
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  todoText: {
+    color: 'white',
+  },
+  todoDelete: {
+    color: 'red',
   },
 });
-
-export default App;
